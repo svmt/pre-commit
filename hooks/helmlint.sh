1a4dbe403ccaf1b334f6bdf9d546a5d915ad71da
Sergiy Mashtaler <sergiy.mashtaler@digib.com>
2020-11-19 09:52:12 +0100

    tets

--- hooks/helmlint.sh
+++ hooks/helmlint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+PRECOMMIT_DEBUG=1
 
 # Run helm lint on the chart path.
 # A typical helm chart directory structure looks as follows:
@@ -59,6 +60,21 @@ debug() {
   fi
 }
 
+debug "$@"
+
+for file in "$@"; do
+  debug "File: $file"
+  # debug "Checking $file"
+  # $file_chart_path=$(chart_path "$file")
+  # debug "Resolved $file to chart path $file_chart_path"
+  # if [[ ! -z "$file_chart_path" ]]; then
+  #   break
+  # fi
+done
+
+exit 1
+
+
 # Recursively walk up the tree until the current working directory and check if the changed file is part of a helm
 # chart. Helm charts have a Chart.yaml file.
 chart_path() {
@@ -97,38 +113,20 @@ chart_path() {
     echo "$changed_file_dir"
     return 0
   fi
-
-  # None of the above, so recurse and do again in the parent dir
-  chart_path "$changed_file_dir"
 }
-
-# An array to keep track of which charts we already linted
-seen_chart_paths=()
 
 for file in "$@"; do
   debug "Checking $file"
-  file_chart_path=$(chart_path "$file")
+  $file_chart_path=$(chart_path "$file")
   debug "Resolved $file to chart path $file_chart_path"
-
-  # The chart values.yaml file may not have all the values defined to enforce default values, which will cause the
-  # linter to fail. To support this, this pre-commit hook looks for a special values file called `linter_values.yaml`
-  # which should define the additional values that will be fed to the linter.
-  if [[ -f "$file_chart_path/linter_values.yaml" ]]; then
-    linter_values_arg="$file_chart_path/linter_values.yaml"
-  else
-    linter_values_arg=""
-  fi
-
   if [[ ! -z "$file_chart_path" ]]; then
-    if contains_element "$file_chart_path" "${seen_chart_paths[@]}"; then
-      debug "Already linted $file_chart_path"
-    elif [[ -z "$linter_values_arg" ]]; then
-      helm lint "$file_chart_path"
-      seen_chart_paths+=( "$file_chart_path" )
-    else
-      # Combine both linter_values.yaml and values.yaml
-      helm lint -f "$file_chart_path/values.yaml" -f "$linter_values_arg" "$file_chart_path"
-      seen_chart_paths+=( "$file_chart_path" )
-    fi
+    break
   fi
 done
+
+for file in $(ls "$file_chart_path" | grep -E "values-.*\.yaml"); do
+  debug "$file"
+  helm lint -f "$file_chart_path/values.yaml" -f "$file_chart_path/$file" "$file_chart_path"
+done
+
+exit 1
